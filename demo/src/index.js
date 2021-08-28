12b2c6656afd8b6652cd4149157034804e91232e
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-08-28 23:52:51 +0300

    refactored and added new syntax

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,38 +1,44 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css';
+
+const books = [
+  {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/41tyokViuNL._AC_SX184_.jpg',
+    title: 'The Very Hungry Caterpillar',
+    author: 'Bill Martin Jr.'
+  },
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/41x7bwd0kDS._AC_SX184_.jpg',
+    title: 'A Letter From Your Teacher: On the First Day of School',
+    author: 'Eric Carle'
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51430n+9jlL._AC_SX184_.jpg',
+    title: 'Brown Bear, Brown Bear, What Do You See?',
+    author: 'Shannon Olsen'
+  }
+];
 
 function Booklist() {
   return (
     <section className='booklist'>
-      <Book />
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />
+      })}
     </section>
   );
 }
 
-const images = [
-  'https://images-na.ssl-images-amazon.com/images/I/41tyokViuNL._AC_SX184_.jpg',
-  'https://images-na.ssl-images-amazon.com/images/I/41x7bwd0kDS._AC_SX184_.jpg',
-  'https://images-na.ssl-images-amazon.com/images/I/51430n+9jlL._AC_SX184_.jpg'
-];
-
-const titles = [
-  'The Very Hungry Caterpillar',
-  'A Letter From Your Teacher: On the First Day of School',
-  'Brown Bear, Brown Bear, What Do You See?'
-];
-
-const authors = [
-  'Eric Carle',
-  'Shannon Olsen',
-  'Bill Martin Jr.'
-];
-
-const Book = () => {
+const Book = ({img, title, author}) => {
   return (
-  <article>
-    <img className='cover' src="https://images-na.ssl-images-amazon.com/images/I/41tyokViuNL._AC_SX184_.jpg" alt="The Very Hungry Caterpillar" />
-    <h1 className='title'>The Very Hungry Caterpillar</h1>
-    <h4 className='author'>by Eric Carle</h4>
+  <article className='book'>
+    <img className='cover' src={img} alt='' />
+    <h1 className='title'>{title}</h1>
+    <h4 className='author'>by {author}</h4>
   </article>
   );
 }
