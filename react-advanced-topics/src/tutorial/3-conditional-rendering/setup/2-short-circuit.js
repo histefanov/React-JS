7267ff00fa2ef2cdfad42066f63e3ee6b2440c46
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-08-30 15:23:40 +0300

    added examples of short-circuit evaluations and ternary operators in the context of jsx

--- react-advanced-topics/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-topics/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -6,7 +6,20 @@ const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  const [isError , setIsError] = useState(false);
+
+  return (
+    <>
+      <h3>short-circuit and ternary operator evaluations</h3>
+      <button className='btn' style={{marginBottom: '2rem'}} onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      <div>       
+        <h4>Short-circuit evaluation: {isError && 'Error!'}</h4>
+        <h4>Ternary operator: {isError ? 'Error!' : 'No error.'}</h4>
+      </div>
+    </>
+  )
 };
 
 export default ShortCircuit;
