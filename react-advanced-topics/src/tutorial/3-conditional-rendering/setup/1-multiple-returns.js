d42294c8dc0ac4720c51fae40f86b290393e0ef3
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-08-30 13:31:42 +0300

    added example for conditional rendering with multiple returns

--- react-advanced-topics/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced-topics/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,45 @@
 import React, { useState, useEffect } from 'react';
+
 const url = 'https://api.github.com/users/QuincyLarson';
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        }
+        else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(resp.statusText);
+        }
+      })
+      .then((user) => {
+        setUser(user);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  })
+
+  if (isLoading) {
+    return <h1>Loading...</h1>
+  }
+
+  if (isError) {
+    return <h1>Error!</h1>
+  }
+
+  return (
+    <div>
+      <img src={user.avatar_url} alt={user.login} style={{borderRadius: '50%'}}/>
+      <h3>{user.login}</h3>
+    </div>    
+  )
 };
 
 export default MultipleReturns;
