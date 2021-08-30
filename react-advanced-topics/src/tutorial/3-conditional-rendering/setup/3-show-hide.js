8ddc54013dba523227204376fbd9d49fad01dc88
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-08-30 16:04:50 +0300

    added example of component show/hide functionality while using useState and useEffect hooks

--- react-advanced-topics/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ react-advanced-topics/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,33 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [isVisible, setIsVisible] = useState(false);
+
+  return <>
+    <button className='btn' onClick={() => setIsVisible(!isVisible)}>
+      show/hide
+    </button>
+    {isVisible && <Item />}
+  </>;
 };
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize);
+    return () => {
+      window.removeEventListener('resize', checkSize)
+    }
+  }, [])
+
+  return <div style={{marginTop: '2rem'}}>
+    Width: {size} PX
+  </div>
+}
 
 export default ShowHide;
