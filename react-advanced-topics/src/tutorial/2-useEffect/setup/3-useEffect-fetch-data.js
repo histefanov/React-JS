74bff1164b006a59706df04f50e776783ed6c8f9
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-08-30 12:00:46 +0300

    added useEffect example for fetching data asynchronously

--- react-advanced-topics/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ react-advanced-topics/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,35 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+
+  const GetUsers = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setUsers(data);
+  } 
+
+  useEffect(() => {
+    GetUsers();
+  }, [])
+
+  return (
+    <>
+      <h2>github users</h2>
+      <ul className='users'>
+        {users.map((user) => {
+          const {id, login, avatar_url, html_url} = user;
+          return <li key={id}>
+            <img src={avatar_url} alt={`${login}'s avatar`} />
+            <div>
+              <h4>{login}</h4>
+              <a href={html_url}>profile</a>
+            </div>
+          </li>
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
