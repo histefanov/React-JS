b4433bf31a3de4091791846a6146fb02c30219ee
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-09-08 22:29:29 +0300

    added example of useCallback and useMemo

--- react-advanced-topics/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ react-advanced-topics/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -5,16 +5,40 @@ const url = "https://course-api.com/javascript-store-products";
 
 // every time props or state changes, component re-renders
 
+const calculateMostExpensiveItem = (data) => {
+  return (
+    data.reduce((total, item) => {
+      const price = item.fields.price;
+      if (price > total) {
+        total = price;
+      }
+      return total;
+    }, 0) / 100
+  );
+};
+
 const Index = () => {
   const { products } = useFetch(url);
   const [count, setCount] = useState(0);
 
+  const incrementCounter = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+
+  const mostExpensiveItem = useMemo(
+    () => calculateMostExpensiveItem(products),
+    [products]
+  );
+
   return (
     <>
       <h1>Count : {count}</h1>
-      <button className='btn' onClick={() => setCount(count + 1)}>
+      <button className='btn' onClick={() => incrementCounter()}>
         click me
       </button>
+      <h3 style={{ marginTop: "3rem" }}>
+        Most expensive : ${mostExpensiveItem}
+      </h3>
       <BigList products={products} />
     </>
   );
@@ -34,6 +58,8 @@ const SingleProduct = ({ fields }) => {
   let { name, price } = fields;
   price = price / 100;
   const image = fields.image[0].url;
+
+  console.count("single item");
 
   return (
     <article className='product'>
