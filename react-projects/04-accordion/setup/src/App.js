4b1e08e98ad85c86fdc7ae2f1c3bebbeba24eba8
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-09-10 17:32:01 +0300

    added expandable questions project

--- react-projects/04-accordion/setup/src/App.js
+++ react-projects/04-accordion/setup/src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <div className='container'>
+        <h3>FAQ About Login</h3>
+        <section className='info'>
+          {data.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
