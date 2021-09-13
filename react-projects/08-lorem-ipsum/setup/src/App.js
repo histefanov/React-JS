a13ada53d2c265f22d7cc42077eec951e1a1a3ce
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-09-13 10:25:33 +0300

    added text generator project

--- react-projects/08-lorem-ipsum/setup/src/App.js
+++ react-projects/08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,51 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
+
 function App() {
-  
-  
+  const [count, setCount] = useState(null);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let amount = parseInt(count);
+
+    if (count < 0) {
+      amount = 1;
+    }
+
+    if (count > data.length) {
+      amount = data.length;
+    }
+
+    setText(data.slice(0, amount));
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <main>
+      <section className='section-center'>
+        <h3>Tired of boring lorem ipsum?</h3>
+        <form className='lorem-form' onSubmit={handleSubmit}>
+          <label htmlFor='count'>Paragraphs:</label>
+          <input
+            id='count'
+            type='number'
+            name='count'
+            value={count}
+            onChange={(e) => setCount(e.target.value)}
+          />
+          <button type='submit' className='btn'>
+            Generate
+          </button>
+        </form>
+        <article className='lorem-text'>
+          {text.map((par, index) => {
+            return <p key={index}>{par}</p>;
+          })}
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default App;
